635ffe96f591e4e84c80981c44bb2f8bcf51ca8a
livefromthelevel <brian.d.wach@gmail.com>
2024-03-10 12:53:53 -0500

    <Initial list selction inquirier logic is working>

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 const inquirer = require('inquirer');
-const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
-inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
 
-// Each prompt is an array fed into Inquirer to be prompt object members.
-// Index 0: type, Index 1: name, Index 2: message, 
-// Index 3: choices, Index 4: loop, Index 5: maxLength, Index 6:validate
-const prompts = [
-    ['maxlength-input', 'text', 'Enter text for logo:', '', '', '3'],
-    ['input', 'textColor', 'Enter color for text (ex. purple, or #800080):', '', '', '', validateColor],
-    ['list', 'shape', 'Choose a shape:', shapes.shapesArray, false],
-    ['input', 'shapeColor', 'Enter color for shape (ex. red, or #FF0000):', '', '', '', validateColor]
+const initPromptListArr = [
+    'View All Departments',
+    'View All Roles',
+    'View All Employees',
+    //'Add Department',
+    //'Add Role',
+    //'Add Employee', 
+    //'Update Employee Role'
 ];
 
-// Begins asynchronous iterations of Inquirer prompts for the user
-async function startPromptsAsync(prompts) {
-    const promptsArray = [];
-    for (prompt of prompts) {
-        const [type, name, message, choices, loop, maxLength, validate] = prompt;
-        promptsArray.push({ type: type, name: name, message: message, choices: choices, loop: loop, maxLength: maxLength, validate: validate });
-    };
+const initPromptList = {
+    type: 'list',
+    message: 'What would you like to do?',
+    name: 'action',
+    choices: initPromptListArr,
+    loop: false
+}
 
-    const response = await promptForUserInput(promptsArray);
-
-    compileRender(response);
+function initPrompt(initPromptList) {
+    return inquirer.prompt(initPromptList);
 };
 
-// Inquirer function for collecting user input
-function promptForUserInput(currentPrompt) {
-    return inquirer.prompt(currentPrompt);
-};
+async function getUserActionAsync() {
+    const response = await initPrompt(initPromptList);
+    switch (response.action) {
+        case 'View All Departments':
+            console.log('View All Departments');
+            break;
+        case 'View All Roles':
+            console.log('View All Roles');
+            break;
+        case 'View All Employees':
+            console.log('View All Employees');
+            break;
+    }
+}
 
 function init() {
-    startPromptsAsync(prompts);
+    console.log('WELCOME TO EMPLOYEE MANAGER');
+    getUserActionAsync();
 };
 
 // Calls initiliazation of application
