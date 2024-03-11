410af92fc4f46f01d45e98062a2fcd2cb4185726
livefromthelevel <brian.d.wach@gmail.com>
2024-03-11 14:33:22 -0500

    <Application prompts for next action after results are rendered>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 
-const viewData = require('./lib/view.js');
+const viewDataAsync = require('./lib/view.js');
 
 const initPromptListArr = [
     'View All Departments',
@@ -20,33 +20,38 @@ const initPromptList = {
     loop: false
 };
 
-function initPrompt(initPromptList) {
-    return inquirer.prompt(initPromptList);
+async function getUserActionAsync() {
+    const response = await inquirer.prompt(initPromptList);
+    userActionRouterAsync(response);
 };
 
-async function getUserActionAsync() {
-    const response = await initPrompt(initPromptList);
+async function userActionRouterAsync(response) {
+    var sql = '';
     switch (response.action) {
         case initPromptListArr[0]:
-            viewData(initPromptListArr, initPromptListArr[0]);
+            await viewDataAsync(initPromptListArr, initPromptListArr[0]);
             break;
         case initPromptListArr[1]:
-            viewData(initPromptListArr, initPromptListArr[1]);
+            await viewDataAsync(initPromptListArr, initPromptListArr[1]);
             break;
         case initPromptListArr[2]:
-            viewData(initPromptListArr, initPromptListArr[2]);
+            await viewDataAsync(initPromptListArr, initPromptListArr[2]);
             break;
     }
+    getUserActionAsync();
 };
 
 function init() {
     console.log(`
-8"""" 8""8""8 8""""8 8     8"""88 8    8 8"""" 8""""    8""8""8 8""""8 8"""8 8""""8 8""""8 8"""" 8"""8  
-8     8  8  8 8    8 8     8    8 8    8 8     8        8  8  8 8    8 8   8 8    8 8    " 8     8   8  
-8eeee 8e 8  8 8eeee8 8e    8    8 8eeee8 8eeee 8eeee    8e 8  8 8eeee8 8e  8 8eeee8 8e     8eeee 8eee8e 
-88    88 8  8 88     88    8    8   88   88    88       88 8  8 88   8 88  8 88   8 88  ee 88    88   8 
-88    88 8  8 88     88    8    8   88   88    88       88 8  8 88   8 88  8 88   8 88   8 88    88   8 
-88eee 88 8  8 88     88eee 8eeee8   88   88eee 88eee    88 8  8 88   8 88  8 88   8 88eee8 88eee 88   8 
+ _____________________________________________________________________________________________________________
+|                                                                                                             |
+|   8"""" 8""8""8 8""""8 8     8"""88 8    8 8"""" 8""""    8""8""8 8""""8 8"""8 8""""8 8""""8 8"""" 8"""8    |
+|   8     8  8  8 8    8 8     8    8 8    8 8     8        8  8  8 8    8 8   8 8    8 8    " 8     8   8    |
+|   8eeee 8e 8  8 8eeee8 8e    8    8 8eeee8 8eeee 8eeee    8e 8  8 8eeee8 8e  8 8eeee8 8e     8eeee 8eee8e   |
+|   88    88 8  8 88     88    8    8   88   88    88       88 8  8 88   8 88  8 88   8 88  ee 88    88   8   |
+|   88    88 8  8 88     88    8    8   88   88    88       88 8  8 88   8 88  8 88   8 88   8 88    88   8   |
+|   88eee 88 8  8 88     88eee 8eeee8   88   88eee 88eee    88 8  8 88   8 88  8 88   8 88eee8 88eee 88   8   |
+|_____________________________________________________________________________________________________________|
 `);
     getUserActionAsync();
 };
